578fd789e899b0bfee22a10dd5a1786a75dba514
Vinicius martins <viniciusmartinsgsi@gmail.com>
2024-08-23 10:41:43 -0300

    Ultima alteração do db_config

--- config/conexao.php
+++ config/conexao.php
@@ -3,9 +3,9 @@
 if (!defined('DB_CONFIG')) {
     define('DB_CONFIG', [
         'host' => 'localhost',
-        'dbname' => 'new_agenda_2024',
-        'user' => 'root',
-        'pass' => 'bdjmf'
+        'dbname' => '',
+        'user' => '',
+        'pass' => ''
     ]);
 }
 
